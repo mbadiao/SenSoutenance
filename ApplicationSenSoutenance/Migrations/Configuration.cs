6af8e60f5d9ab6cdfbfdd447911a7fe66457e175
Yandé Guissé <yandeiguiss2@gmail.com>
2026-01-11 21:24:54 +0000

    configuration de la connexion avec mysql workbench

--- ApplicationSenSoutenance/Migrations/Configuration.cs
+++ ApplicationSenSoutenance/Migrations/Configuration.cs
@@ -5,15 +5,15 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationSenSoutenance.Models.BdSenSoutenanceContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationSenSoutenance.Models.dbsensoutenanceContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationDataLossAllowed = false;
         }
 
-        protected override void Seed(ApplicationSenSoutenance.Models.BdSenSoutenanceContext context)
+        protected override void Seed(ApplicationSenSoutenance.Models.dbsensoutenanceContext context)
         {
             //  This method will be called after migrating to the latest version.
 
---
+++ ApplicationSenSoutenance/Models/dbsensoutenanceContext.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationSenSoutenance.Models
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class dbsensoutenanceContext:DbContext
+    {
+        public dbsensoutenanceContext() : base("connBdSenSoutenance") { }
+        public DbSet<AnneeAcademique> AnneeAcademiques { get; set; }
+
+
+    }
+}
